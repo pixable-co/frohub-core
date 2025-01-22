2d0ce5948183ee5ada7c5d37ff9f9bdeb9ff6a5b
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-01-22 22:19:27 +0600

    Updated

--- wp-content/plugins/frohub-core/src/main.jsx
+++ wp-content/plugins/frohub-core/src/main.jsx
@@ -2,10 +2,3 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div>
-        <h1>Alhamdulillah</h1>
-    </div>
-  </StrictMode>,
-)
