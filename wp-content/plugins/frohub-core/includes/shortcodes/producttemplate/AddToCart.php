338d5d7c9e6af342f0083122f14e4d21fb34168a
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-01-29 13:41:36 +0600

    Added Partner ID on Data

--- wp-content/plugins/frohub-core/includes/shortcodes/producttemplate/AddToCart.php
+++ wp-content/plugins/frohub-core/includes/shortcodes/producttemplate/AddToCart.php
@@ -15,9 +15,14 @@ class RenderAddToCart {
     public function frohub_add_to_cart_shortcode() {
         global $post;
         $product_id = $post->ID;
+        $partner_id = get_field('partner_id', $product_id);
 
         $unique_key = 'frohub_add_to_cart' . uniqid();
-        return '<div class="frohub_add_to_cart" data-key="' . esc_attr($unique_key) . '" data-product-id="' . esc_attr($product_id) . '"></div>';
+        return '<div class="frohub_add_to_cart"
+                        data-key="' . esc_attr($unique_key) . '"
+                        data-product-id="' . esc_attr($product_id) . '"
+                        data-partner-id="' . esc_attr($partner_id) . '">
+                    </div>';
     }
 }
 
