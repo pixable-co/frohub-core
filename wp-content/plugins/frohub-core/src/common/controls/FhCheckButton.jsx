b7e6d6ba3cdea1efbcfa2f4ee5bb449456ddff9b
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-03-03 14:46:35 +0600

    Updated

--- wp-content/plugins/frohub-core/src/common/controls/FhCheckButton.jsx
+++ wp-content/plugins/frohub-core/src/common/controls/FhCheckButton.jsx
@@ -16,7 +16,7 @@ const FhCheckButton = ({ addOn, selectedAddOns, handleSelectAddOn, loading }) =>
     return (
         <label
             className={`inline-flex items-center cursor-pointer px-4 py-2 border rounded-full ${
-                selectedAddOns.includes(addOn) ? "bg-gray-300 border-gray-500" : "border-gray-300"
+                selectedAddOns.includes(addOn) ? "fhcheck-selected border-gray-500" : "border-gray-300"
             }`}
         >
             <input
