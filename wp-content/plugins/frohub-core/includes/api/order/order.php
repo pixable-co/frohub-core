5c7e96300e693256188d7f309a856292eb2e419d
issaKebin <146450805+issaKebin@users.noreply.github.com>
2025-03-20 18:36:27 +0000

    Update order.php

--- wp-content/plugins/frohub-core/includes/api/order/order.php
+++ wp-content/plugins/frohub-core/includes/api/order/order.php
@@ -38,8 +38,6 @@ class Order {
 
 
         wc_delete_shop_order_transients($order_id); // Clears cached order data
-        clean_post_cache($order_id);
-        wp_cache_delete($order_id, 'orders');
 
         $order = wc_get_order($order_id);
         $order->read(); // Force WooCommerce to reload the latest status
