431923ee12e23f1f8a4c0642a287c7d3f54f2710
Kevin La <46628649+Kevlar0150@users.noreply.github.com>
2025-04-14 11:41:21 +0100

    Update frohub_get_partner_location.php

--- wp-content/plugins/frohub-core/includes/shortcodes/producttemplate/frohub_get_partner_location.php
+++ wp-content/plugins/frohub-core/includes/shortcodes/producttemplate/frohub_get_partner_location.php
@@ -1,7 +1,7 @@
 <?php
 namespace FECore;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -9,51 +9,50 @@ class FrohubGetPartnerLocation {
 
     public static function init() {
         $self = new self();
-        add_shortcode( 'frohub_get_partner_location', array($self, 'frohub_get_partner_location_shortcode') );
+        add_shortcode('frohub_get_partner_location', array($self, 'frohub_get_partner_location_shortcode'));
     }
 
     public function frohub_get_partner_location_shortcode() {
-        global $post;
-    
-        if ( ! $post instanceof \WP_Post ) {
+        global $product;
+
+        if (!$product || !$product->is_type('variable')) {
             return '';
         }
-    
-        // Check if this is a variation
-        $product = wc_get_product( $post->ID );
-    
-        if ( ! $product || ! $product->is_type('variation') || $product->get_status() !== 'publish' ) {
+
+        $variation_ids = $product->get_children();
+        $matched = false;
+
+        foreach ($variation_ids as $variation_id) {
+            $variation = wc_get_product($variation_id);
+
+            if (!$variation || $variation->get_status() !== 'publish') {
+                continue;
+            }
+
+            $service_type = strtolower($variation->get_attribute('pa_service-type'));
+
+            if (in_array($service_type, ['home-based', 'salon-based'])) {
+                $matched = true;
+                break;
+            }
+        }
+
+        if (!$matched) {
             return '';
         }
-    
-        // Get the pa_service-type attribute
-        $service_type = $product->get_attribute('pa_service-type');
-    
-        // Check if it's home-based or salon-based (case insensitive)
-        if ( ! in_array( strtolower($service_type), ['home-based', 'salon-based'] ) ) {
+
+        $partner_id = get_field('partner_id', $product->get_id());
+
+        if (!$partner_id) {
             return '';
         }
-    
-        // Get partner ID from variation or parent product
-        $partner_id = get_field('partner_id', $product->get_id()); // Try variation-level first
-    
-        if ( ! $partner_id ) {
-            $parent_id = $product->get_parent_id();
-            $partner_id = get_field('partner_id', $parent_id); // Fallback to parent product
-        }
-    
-        if ( ! $partner_id ) {
-            return '';
-        }
-    
-        // Get and return postcode
+
         $postcode = get_field('postcode', $partner_id);
-    
-        if ( ! $postcode ) {
+
+        if (!$postcode) {
             return '';
         }
-    
+
         return '<span class="frohub-partner-location"><i class="fas fa-map-marker-alt"></i> ' . esc_html($postcode) . '</span>';
     }
-    
 }
