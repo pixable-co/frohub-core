07d25168cf944169578c183447c88ff8b30e7597
issaKebin <146450805+issaKebin@users.noreply.github.com>
2025-04-24 15:41:23 +0100

    Update get_order_status.php

--- wp-content/plugins/frohub-core/includes/shortcodes/order/get_order_status.php
+++ wp-content/plugins/frohub-core/includes/shortcodes/order/get_order_status.php
@@ -1,18 +1,22 @@
 <?php
+
 namespace FECore;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
-class GetOrderStatus {
+class GetOrderStatus
+{
 
-    public static function init() {
+    public static function init()
+    {
         $self = new self();
-        add_shortcode( 'get_order_status', array($self, 'get_order_status_shortcode') );
+        add_shortcode('get_order_status', array($self, 'get_order_status_shortcode'));
     }
 
-    public function get_order_status_shortcode() {
+    public function get_order_status_shortcode()
+    {
         ob_start();
 
         $order_id = $GLOBALS['single_order_id'];
@@ -51,7 +55,7 @@ class GetOrderStatus {
                                 <span class="close-modal">×</span>
                             </div>
                             <div class="modal-body">
-                                <p class="confirmation-text">Are you sure you want to cancel this order?</p>
+                                <p class="confirmation-text">Are you sure you want to cancel this booking request?</p>
                             </div>
                             <div class="modal-footer">
                                 <button class="w-btn us-btn-style_6 w-btn-underlined confirm-cancel-order"
@@ -61,6 +65,28 @@ class GetOrderStatus {
                             </div>
                         </div>
                       </div>';
+                echo '<div id="cancelReasonModal_<?php echo esc_attr($order_id); ?>" class="status-modal">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5>Help us improve, tell us why you’re cancelling</h5>
+                            <span class="close-modal">×</span>
+                            </div>
+                            <div class="modal-body">
+                            <p>We’d love to know why you decided to cancel. Your feedback helps us improve the booking experience for you and others.</p>
+                            <form id="cancel-reason-form-<?php echo esc_attr($order_id); ?>">
+                                <label><input type="radio" name="reason" value="scheduling"> I had a scheduling conflict</label><br>
+                                <label><input type="radio" name="reason" value="changed-mind"> I changed my mind</label><br>
+                                <label><input type="radio" name="reason" value="no-response"> The stylist didn’t respond in time</label><br>
+                                <label><input type="radio" name="reason" value="stylist-cancel"> The stylist asked me to cancel</label><br>
+                                <label><input type="radio" name="reason" value="other"> Other</label>
+                                <textarea name="other_reason" placeholder="Enter your reason here..."></textarea>
+                            </form>
+                            </div>
+                            <div class="modal-footer">
+                            <button class="w-btn us-btn-style_6 w-btn-underlined submit-final-cancel" data-order-id="<?php echo esc_attr($order_id); ?>">Cancel Booking</button>
+                            </div>
+                        </div>
+                        </div>';
             } elseif ($status === 'processing') {
                 $start_date = '';
 
@@ -122,106 +148,119 @@ class GetOrderStatus {
                       </div>';
             }
         }
-        ?>
+?>
 
         <script type="text/javascript">
-        jQuery(document).ready(function ($) {
-            $(".modal-trigger").click(function () {
-                var modalId = $(this).data("modal");
-                var orderId = $(this).data("order-id");
+            jQuery(document).ready(function($) {
+                $(".modal-trigger").click(function() {
+                    var modalId = $(this).data("modal");
+                    var orderId = $(this).data("order-id");
 
-                $("#" + modalId).css("display", "block");
-                $("#" + modalId).find(".confirm-early-cancel-order, .confirm-late-cancel-order").attr("data-order-id", orderId);
-            });
+                    $("#" + modalId).css("display", "block");
+                    $("#" + modalId).find(".confirm-early-cancel-order, .confirm-late-cancel-order").attr("data-order-id", orderId);
+                });
 
-            $(".close-modal, .close-modal-text").click(function () {
-                $(".status-modal").css("display", "none");
-            });
+                $(".close-modal, .close-modal-text").click(function() {
+                    $(".status-modal").css("display", "none");
+                });
 
-            $(window).click(function (event) {
-                $(".status-modal").each(function () {
-                    if (event.target === this) {
-                        $(this).css("display", "none");
-                    }
+                $(window).click(function(event) {
+                    $(".status-modal").each(function() {
+                        if (event.target === this) {
+                            $(this).css("display", "none");
+                        }
+                    });
+                });
+
+                function showSpinner(button) {
+                    $(button).prop("disabled", true);
+                    $(button).find(".spinner").removeClass("hidden");
+                }
+
+                function hideSpinner(button) {
+                    $(button).prop("disabled", false);
+                    $(button).find(".spinner").addClass("hidden");
+                }
+
+                $(".confirm-cancel-order").click(function() {
+                    // First modal triggers second modal instead of AJAX directly
+                    var orderId = $(this).data("order-id");
+                    $("#cancelModal_" + orderId).hide();
+                    $("#cancelReasonModal_" + orderId).show();
+                });
+
+                $(".submit-final-cancel").click(function() {
+                    var button = $(this);
+                    var orderId = button.data("order-id");
+                    var form = $("#cancel-reason-form-" + orderId);
+                    var selectedReason = form.find("input[name='reason']:checked").val();
+                    var otherText = form.find("textarea[name='other_reason']").val();
+
+                    showSpinner(button);
+
+                    $.post(ajaxurl, {
+                        action: "cancel_order",
+                        security: "<?php echo wp_create_nonce('ajax_nonce'); ?>",
+                        order_id: orderId,
+                        reason: selectedReason,
+                        other_reason: otherText
+                    }, function(response) {
+                        hideSpinner(button);
+                        if (response.success) {
+                            alert(response.data.message);
+                            location.reload();
+                        } else {
+                            alert(response.data.message);
+                        }
+                    });
+                });
+
+
+                $(".confirm-early-cancel-order").click(function() {
+                    var button = $(this);
+                    var orderId = button.data("order-id");
+
+                    showSpinner(button);
+
+                    $.post("<?php echo admin_url('admin-ajax.php'); ?>", {
+                        action: "early_cancel_order",
+                        security: "<?php echo wp_create_nonce('ajax_nonce'); ?>",
+                        order_id: orderId
+                    }, function(response) {
+                        hideSpinner(button);
+                        if (response.success) {
+                            alert(response.data.message);
+                            location.reload();
+                        } else {
+                            alert(response.data.message);
+                        }
+                    });
+                });
+
+                $(".confirm-late-cancel-order").click(function() {
+                    var button = $(this);
+                    var orderId = button.data("order-id");
+
+                    showSpinner(button);
+
+                    $.post("<?php echo admin_url('admin-ajax.php'); ?>", {
+                        action: "late_cancel_order",
+                        security: "<?php echo wp_create_nonce('ajax_nonce'); ?>",
+                        order_id: orderId
+                    }, function(response) {
+                        hideSpinner(button);
+                        if (response.success) {
+                            alert(response.data.message);
+                            location.reload();
+                        } else {
+                            alert(response.data.message);
+                        }
+                    });
                 });
             });
-
-            function showSpinner(button) {
-                $(button).prop("disabled", true);
-                $(button).find(".spinner").removeClass("hidden");
-            }
-
-            function hideSpinner(button) {
-                $(button).prop("disabled", false);
-                $(button).find(".spinner").addClass("hidden");
-            }
-
-            $(".confirm-cancel-order").click(function () {
-                var button = $(this);
-                var orderId = button.data("order-id");
-
-                showSpinner(button);
-
-                $.post("<?php echo admin_url('admin-ajax.php'); ?>", {
-                    action: "cancel_order",
-                    security: "<?php echo wp_create_nonce('ajax_nonce'); ?>",
-                    order_id: orderId
-                }, function (response) {
-                    hideSpinner(button);
-                    if (response.success) {
-                        alert(response.data.message);
-                        location.reload();
-                    } else {
-                        alert(response.data.message);
-                    }
-                });
-            });
-
-            $(".confirm-early-cancel-order").click(function () {
-                var button = $(this);
-                var orderId = button.data("order-id");
-
-                showSpinner(button);
-
-                $.post("<?php echo admin_url('admin-ajax.php'); ?>", {
-                    action: "early_cancel_order",
-                    security: "<?php echo wp_create_nonce('ajax_nonce'); ?>",
-                    order_id: orderId
-                }, function (response) {
-                    hideSpinner(button);
-                    if (response.success) {
-                        alert(response.data.message);
-                        location.reload();
-                    } else {
-                        alert(response.data.message);
-                    }
-                });
-            });
-
-            $(".confirm-late-cancel-order").click(function () {
-                var button = $(this);
-                var orderId = button.data("order-id");
-
-                showSpinner(button);
-
-                $.post("<?php echo admin_url('admin-ajax.php'); ?>", {
-                    action: "late_cancel_order",
-                    security: "<?php echo wp_create_nonce('ajax_nonce'); ?>",
-                    order_id: orderId
-                }, function (response) {
-                    hideSpinner(button);
-                    if (response.success) {
-                        alert(response.data.message);
-                        location.reload();
-                    } else {
-                        alert(response.data.message);
-                    }
-                });
-            });
-        });
         </script>
 
-        <?php
+<?php
         return ob_get_clean();
     }
 }
