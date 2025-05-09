650aa0a52d0e2c04bd5076b466eae76a22eaa446
Kevin La <46628649+Kevlar0150@users.noreply.github.com>
2025-05-09 14:24:01 +0100

    Update get_order_beautician_details.php

--- wp-content/plugins/frohub-core/includes/shortcodes/order/get_order_beautician_details.php
+++ wp-content/plugins/frohub-core/includes/shortcodes/order/get_order_beautician_details.php
@@ -18,19 +18,23 @@ class GetOrderBeauticianDetails {
         $order_id = $GLOBALS['single_order_id'];
         $order = wc_get_order($order_id);
 
+        $conversation = get_field('conversation', $order_id);
+        $conversation_id = $conversation->ID;
+        $conversation_url = get_permalink($conversation_id);
+
         if ($order) {
             foreach ($order->get_items() as $item) {
                 $product_id = $item->get_product_id();
                 if ($product_id != 28990) {
                     $partner_id = get_field('partner_id', $product_id);
                     $partner_title = get_the_title($partner_id);
-                    $email = get_field('partner_email', $partner_id);
+                    
                     $phone = get_field('phone', $partner_id);
 
                     if ($partner_id) {
                         echo '<span>' . esc_html($partner_title) . '</span>   ';
-                        if ($email) {
-                            echo '<a href="mailto:' . esc_attr($email) . '"><i class="fas fa-envelope"></i></a>';
+                        if ($conversation_url) {
+                            echo '<a href="'. esc_attr($conversation_url) . '"><i class="fas fa-envelope"></i></a>';
                         }
                         echo '<br>';
                         if ($phone) {
