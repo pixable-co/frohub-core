036778a7af5908ddee18aa73555d148efc40e232
issaKebin <146450805+issaKebin@users.noreply.github.com>
2025-05-29 13:55:31 +0100

    Update check_add_on_slug_availability.php

--- wp-content/plugins/frohub-core/includes/api/addons/check_add_on_slug_availability.php
+++ wp-content/plugins/frohub-core/includes/api/addons/check_add_on_slug_availability.php
@@ -18,28 +18,9 @@ class CheckAddOnSlugAvailability {
     public function register_rest_routes() {
         register_rest_route('frohub/v1', '/check-add-on-slug-availability', array(
             'methods'             => 'GET',
-            'callback'            => array($this, 'handle_request'),
-            'permission_callback' => '__return_true',
-        ));
-
-        register_rest_route('frohub/v1', '/check-addon', array(
-            'methods'             => 'GET',
             'callback'            => array($this, 'handle_addon_check'),
             'permission_callback' => '__return_true',
         ));
-    }
-
-    /**
-     * Handles the /check-add-on-slug-availability API request.
-     *
-     * @param \WP_REST_Request $request
-     * @return \WP_REST_Response
-     */
-    public function handle_request(\WP_REST_Request $request) {
-        return new \WP_REST_Response(array(
-            'success' => true,
-            'message' => 'check-add-on-slug-availability API endpoint reached',
-        ), 200);
     }
 
     /**
