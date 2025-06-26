ee6e07f668c65d923b2d7dc576520794e7ad0cd1
Kevin La <46628649+Kevlar0150@users.noreply.github.com>
2025-06-26 12:04:15 +0100

    Update create_review_post.php

--- wp-content/plugins/frohub-core/includes/actions/gravity-forms/create_review_post.php
+++ wp-content/plugins/frohub-core/includes/actions/gravity-forms/create_review_post.php
@@ -85,7 +85,7 @@ class CreateReviewPost
             $payload = array(
                 'partner_email'     => $partner_email,
                 'client_first_name' => $customer_first_name,
-                'partner_name'      => $partner_name . 
+                'partner_name'      => $partner_name 
             );
 
             $response = wp_remote_post('https://flow.zoho.eu/20103370577/flow/webhook/incoming?zapikey=1001.e83523e791d77d7d52578d8a6bf2d8fe.2bd19f022b6f6c88bbf0fa6d7da05c4d&isdebug=false', array(
